dfe15c83e8958ad27c4b4b0db210cb107f9951b7
Alex <850946554@qq.com>
2016-12-09 23:18:31 +0800

    Daily write code.--2016.12.09

---
+++ app/src/main/java/com/alex/utils/URLs.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2016 Alex_ZHOU
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alex.utils;
+
+public class URLs {
+
+    // My location server address.
+    private static final String LOCATION_SERVER = "http://192.168.1.109:8080/VMServer/";
+
+
+    // Using address.
+    private static final String SERVER_ADDRESS = LOCATION_SERVER;
+
+
+    // 登陆访问的地址
+    public static final String URL_LOGIN = SERVER_ADDRESS + "Login";
+}
--- app/src/main/java/com/alex/vmandroid/display/main/fragments/LoginFragment.java
+++ app/src/main/java/com/alex/vmandroid/display/main/fragments/LoginFragment.java
@@ -22,22 +22,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.alex.utils.FragmentNavigator;
+import com.alex.utils.URLs;
 import com.alex.vmandroid.R;
 import com.alex.vmandroid.base.BaseFragment;
 import com.alex.vmandroid.display.main.MainContract;
 import com.alex.vmandroid.entities.User;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import rx.Observable;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class LoginFragment extends BaseFragment implements MainContract.LoginView, View.OnClickListener {
 
     private final String TAG = Base_TAG;
 
     private MainContract.MainPresenter mPresenter;
+
+    private EditText mPassword;
+
+    private EditText mAccount;
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
@@ -75,40 +92,36 @@ public class LoginFragment extends BaseFragment implements MainContract.LoginVie
 
         button.setOnClickListener(this);
 
+        mPassword = (EditText) view.findViewById(R.id.main_login_password_et);
+
+        mAccount = (EditText) view.findViewById(R.id.main_login_account_et);
+
 
         // 观察者
-        Subscriber<User> observer = new Subscriber<User>() {
+        observer = new Subscriber<User>() {
             @Override
             public void onCompleted() {
-
+                Log.i(TAG, "onCompleted");
             }
 
             @Override
             public void onError(Throwable e) {
-
+                Log.i(TAG, "onError");
             }
 
             @Override
             public void onNext(User user) {
-
+                Log.i(TAG, "onNext");
+                Toast.makeText(getContext(), user.getUserName() + user.getPassword(), Toast.LENGTH_LONG).show();
             }
         };
-
-        // 被观察者
-        Observable observable = Observable.create(new Observable.OnSubscribe<User>() {
-
-            @Override
-            public void call(Subscriber<? super User> subscriber) {
-                subscriber.onNext(new User());
-                subscriber.onCompleted();
-            }
-        });
-        // 定阅
-        observable.subscribe(observer);
 
 
         return view;
     }
+
+    Subscriber<User> observer;
+
 
     @Override
     public void setPresenter(MainContract.MainPresenter mainPresenter) {
@@ -118,6 +131,54 @@ public class LoginFragment extends BaseFragment implements MainContract.LoginVie
     @Override
     public void onClick(View view) {
         mPresenter.onClick(view.getId());
+        // 定阅
+        //observable.subscribe(observer);
+
+        final String username = mAccount.getText().toString();
+        final String password = mPassword.getText().toString();
+
+        Observable observable = Observable.create(new Observable.OnSubscribe<User>() {
+
+            @Override
+            public void call(final Subscriber<? super User> subscriber) {
+
+                subscriber.onNext(new User());
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody requestBodyPost = new FormBody.Builder()
+                        .add("loginAccount", username)
+                        .add("loginPwd", password)
+                        .build();
+
+                Request requestPost = new Request.Builder()
+                        .url(URLs.URL_LOGIN)
+                        .post(requestBodyPost)
+                        .build();
+
+                client.newCall(requestPost).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        // 获取返回的json数据
+                        final String string = response.body().string();
+                        Log.i(TAG, "onResponse: " + string);
+
+                        User user = new User();
+                        user.setPassword(password);
+                        user.setUserName(username);
+                        subscriber.onNext(user);
+                    }
+                });
+
+            }
+
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+        observable.subscribe(observer);
     }
 
     /**
@@ -126,6 +187,6 @@ public class LoginFragment extends BaseFragment implements MainContract.LoginVie
     @Override
     public void showMainFragment() {
         Log.i(TAG, "showMainFragment");
-        FragmentNavigator.moveTo(getFragmentManager(), R.id.main_frame_layout, UnLoginFragment.class, false, getContext(), null);
+        //FragmentNavigator.moveTo(getFragmentManager(), R.id.main_frame_layout, UnLoginFragment.class, false, getContext(), null);
     }
 }
